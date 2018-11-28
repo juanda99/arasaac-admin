b9d84e34c100ecfa655459acaf1865eca965287b
juanda <juandacorreo@gmail.com>
2018-11-28 14:05:56 +0100

    need styles from material-ui autosuggest

--- app/components/SearchField/index.js
+++ app/components/SearchField/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
+import InputAdornment from '@material-ui/core/InputAdornment'
 import SearchIcon from '@material-ui/icons/Search'
+import ClearIcon from '@material-ui/icons/Clear'
 import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
 import MenuItem from '@material-ui/core/MenuItem'
 import Autosuggest from 'react-autosuggest'
 import match from 'autosuggest-highlight/match'
@@ -12,49 +15,7 @@ import TextField from '@material-ui/core/TextField'
 import styles from './styles'
 import getSuggestions from './filter'
 
-const renderInputComponent = inputProps => {
-  const { classes, inputRef = () => {}, ref, ...other } = inputProps
-  return (
-    <TextField
-      fullWidth
-      InputProps={{
-        inputRef: node => {
-          ref(node)
-          inputRef(node)
-        },
-        classes: {
-          input: classes.input,
-        },
-      }}
-      {...other}
-    />
-  )
-}
-
-const getSuggestionValue = suggestion => suggestion // could be an array of objectsç
-
-const renderSuggestion = (suggestion, { query, isHighlighted }) => {
-  const matches = match(suggestion, query)
-  const parts = parse(suggestion, matches)
-  return (
-    <MenuItem selected={isHighlighted} component="div">
-      <div>
-        {parts.map(
-          (part, index) =>
-            part.highlight ? (
-              <span key={String(index)} style={{ fontWeight: 500 }}>
-                {part.text}
-              </span>
-            ) : (
-              <strong key={String(index)} style={{ fontWeight: 300 }}>
-                {part.text}
-              </strong>
-            ),
-        )}
-      </div>
-    </MenuItem>
-  )
-}
+const getSuggestionValue = suggestion => suggestion // could be an array of objects
 
 class SearchField extends Component {
   state = {
@@ -66,6 +27,79 @@ class SearchField extends Component {
     if (nextProps.value !== this.props.value) {
       this.setState({ searchText: nextProps.value })
     }
+  }
+
+  renderSuggestion = (suggestion, { query, isHighlighted }) => {
+    const classes = this.props
+    const matches = match(suggestion, query)
+    const parts = parse(suggestion, matches)
+    return (
+      <MenuItem className={classes.menuItem} selected={isHighlighted} component="div">
+        <div>
+          {parts.map(
+            (part, index) =>
+              part.highlight ? (
+                <span key={String(index)} style={{ fontWeight: 500, listStyleType: 'none' }}>
+                  {part.text}
+                </span>
+              ) : (
+                <strong key={String(index)} style={{ fontWeight: 300, listStyleType: 'none' }}>
+                  {part.text}
+                </strong>
+              ),
+          )}
+        </div>
+      </MenuItem>
+    )
+  }
+
+  renderInputComponent = inputProps => {
+    const { classes, inputRef = () => {}, ref, ...other } = inputProps
+    return (
+      <TextField
+        fullWidth
+        variant="outlined"
+        InputLabelProps={{
+          classes: {
+            root: classes.cssLabel,
+            focused: classes.cssFocused,
+          },
+        }}
+        InputProps={{
+          inputRef: node => {
+            ref(node)
+            inputRef(node)
+          },
+          classes: {
+            input: classes.input,
+            root: classes.cssOutlinedInput,
+            focused: classes.cssFocused,
+            notchedOutline: classes.notchedOutline,
+          },
+          endAdornment: (
+            <InputAdornment position="end">
+              {this.state.searchText ? (
+                <div className={classes.searchButtons}>
+                  <IconButton className={classes.removeIcon} aria-label="Toggle password visibility">
+                    <ClearIcon />
+                  </IconButton>
+                  <Button variant="contained" color="primary" className={classes.button}>
+                    <SearchIcon />
+                  </Button>
+                </div>
+              ) : (
+                <div className={classes.searchButtons}>
+                  <IconButton className={classes.removeIcon} aria-label="Toggle password visibility">
+                    <SearchIcon />
+                  </IconButton>
+                </div>
+              )}
+            </InputAdornment>
+          ),
+        }}
+        {...other}
+      />
+    )
   }
 
   handleChange = (event, { newValue }) => {
@@ -101,43 +135,36 @@ class SearchField extends Component {
     const { classes } = this.props
     const { searchText } = this.state
     const autosuggestProps = {
-      renderInputComponent,
-      suggestions: this.state.suggestions.slice(0, 5),
+      renderInputComponent: this.renderInputComponent,
+      suggestions: this.state.suggestions.slice(0, 10),
       onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
       onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
       getSuggestionValue,
-      renderSuggestion,
+      renderSuggestion: this.renderSuggestion,
     }
     return (
-      <div style={this.props.style}>
-        <div style={{ display: 'flex', wrap: 'nowrap' }}>
-          <Autosuggest
-            {...autosuggestProps}
-            inputProps={{
-              classes,
-              placeholder: 'prueba',
-              value: searchText,
-              onChange: this.handleChange,
-              onKeyDown: this.onKeyDown,
-            }}
-            theme={{
-              container: classes.container,
-              suggestionsContainerOpen: classes.suggestionsContainerOpen,
-              suggestionsList: classes.suggestionsList,
-              suggestion: classes.suggestion,
-            }}
-            renderSuggestionsContainer={options => (
-              <Paper {...options.containerProps} square>
-                {options.children}
-              </Paper>
-            )}
-          />
-          {searchText ? (
-            <Button variant="contained" color="primary" className={classes.button} onClick={this.handleClick}>
-              <SearchIcon className={classes.icon} />
-            </Button>
-          ) : null}
-        </div>
+      <div style={{ display: 'flex', wrap: 'nowrap' }}>
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            placeholder: 'prueba',
+            value: searchText,
+            onChange: this.handleChange,
+            onKeyDown: this.onKeyDown,
+          }}
+          theme={{
+            container: classes.container,
+            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+          }}
+          renderSuggestionsContainer={options => (
+            <Paper {...options.containerProps} square>
+              {options.children}
+            </Paper>
+          )}
+        />
       </div>
     )
   }
--- app/components/SearchField/styles.js
+++ app/components/SearchField/styles.js
@@ -1,9 +1,43 @@
-const styles = () => ({
+const styles = theme => ({
   button: {
-    height: '48px',
+    height: '54px',
   },
-  icon: {
-    height: '48px',
+  searchButtons: {
+    position: 'absolute',
+    right: 0,
+  },
+  searchIcon: {
+    // height: '54px',
+  },
+  input: {
+    flexGrow: 1,
+    borderColor: 'red',
+  },
+  menuItem: {},
+  container: {
+    flexGrow: 1,
+    // border: '1px solid black',
+    // boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px',
+    // height: '48px',
+  },
+  cssOutlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      boxShadow: 'rgba(0, 0, 0, 0.20) 0px 1px 6px, rgba(0, 0, 0, 0.20) 0px 1px 4px',
+      // borderColor: `${theme.palette.primary.main} !important`,
+      border: 'none !important',
+    },
+  },
+
+  cssFocused: {
+    // boxShadow: 'rgba(0, 0, 0, 0.20) 0px 1px 6px, rgba(0, 0, 0, 0.20) 0px 1px 4px',
+    // borderColor: `${theme.palette.primary.main} !important`,
+    // border: 'none !important',
+  },
+
+  notchedOutline: {
+    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px',
+    // borderWidth: '1px',
+    // borderColor: 'green !important',
   },
 })
 
