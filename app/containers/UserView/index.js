0144508722c7a77a5da834bb818553421249ee4e
juanda <juandacorreo@gmail.com>
2019-05-10 20:05:01 +0200

    fix usage moment with locales

--- app/containers/UserView/index.js
+++ app/containers/UserView/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
+import moment from 'moment'
+import 'moment/min/locales'
 import { withStyles } from '@material-ui/core/styles'
 import withWidth from '@material-ui/core/withWidth'
 import View from 'components/View'
 import injectReducer from 'utils/injectReducer'
 import injectSaga from 'utils/injectSaga'
-import { DAEMON } from 'utils/constants'
 import { FormattedMessage } from 'react-intl'
 import { makeUserByIdSelector } from 'containers/UsersView/selectors'
-import { UserForm } from 'components/UserForm'
+import UserForm from 'components/UserForm'
 import reducer from './reducer'
 import saga from './sagas'
 import { user } from './actions'
@@ -33,12 +34,15 @@ class UserView extends React.PureComponent {
 
   render() {
     const { classes, width, selectedUser } = this.props
+    moment.locale('fr')
+    if (selectedUser) var item = moment(1316116057189).fromNow()
     return (
       <View>
+        <p>{item}</p>
         <h1>{<FormattedMessage {...messages.header} />}</h1>
         {selectedUser ? (
           <div>
-            <p>kk</p>
+            <UserForm initialValues={selectedUser} />
           </div>
         ) : (
           <p>Still no data :-(</p>
