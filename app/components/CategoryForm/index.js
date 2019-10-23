7a9db09427123bae8ed975d9ec5422c3606d1329
juanda <juandacorreo@gmail.com>
2019-10-23 22:21:53 +0200

    tags dropdown sorted

--- app/components/CategoryForm/index.js
+++ app/components/CategoryForm/index.js
@@ -29,8 +29,25 @@ export class CategoryForm extends Component {
   componentDidMount() {
     const { tags, intl } = this.props
     const { formatMessage } = intl
-    suggestions = tags.map(tag => ({ label: formatMessage(tagLabels[tag]), value: tag }))
+    suggestions = tags.map(tag => ({ label: formatMessage(tagLabels[tag]), value: tag })).sort(
+      (a, b) =>
+        a.label
+          .toLowerCase()
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '') >
+        b.label
+          .toLowerCase()
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          ? 1
+          : -1,
+    )
+    console.log(suggestions)
     // fix: first time open form, suggestions are loaded after render
+    // suggestions = tags.map(tag => {
+    //   console.log(tag)
+    //   return { label: formatMessage(tagLabels[tag]), value: tag }
+    // })
     this.forceUpdate()
   }
 
